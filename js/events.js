d57d28954f6d6240019aae7fb7b76ea38b3bd8c1
Theo Booth <theorlbooth@googlemail.com>
2020-03-11 14:26:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,14 @@
 //define functions here
 
+function getIt() {
+  $("p").on('click', function(click) {
+    if (click.which == "click"){
+      alert("Hey!")
+    }
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
