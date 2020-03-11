abdbcc048cbe3cb084c509a5d14a6a871cfee7da
Theo Booth <theorlbooth@googlemail.com>
2020-03-11 14:52:45 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function pressIt() {
 function submitIt() {
   $("form").on("submit", function() {
     alert("Your form is going to be submitted now.")
-  }
+  })
 }
 
 $(document).ready(function(){
